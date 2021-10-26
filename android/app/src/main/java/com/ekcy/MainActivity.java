9d22866842acddd8d6f8842e5e698b4b81b485ec
ztaz311 <ztaz31197@gmail.com>
2021-10-26 23:51:39 +0700

    rename

--- android/app/src/main/java/com/ekcy/MainActivity.java
+++ android/app/src/main/java/com/ekcy/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "eKCY";
+    return "eKYC";
   }
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
--- ios/eKYCTests/eKYCTests.m
+++ ios/eKYCTests/eKYCTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface eKCYTests : XCTestCase
+@interface eKYCTests : XCTestCase
 
 @end
 
-@implementation eKCYTests
+@implementation eKYCTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
